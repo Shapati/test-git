1ae98b35744cd1a612ff5033ffae3731a0ae6916
Ayandiran Daniel <nifemiayandiran@gmail.com>
2022-07-14 23:07:23 +0100

    added prev and next buttons for testimonials

--- src/pages/Home/IntroSection/IntroSection.jsx
+++ src/pages/Home/IntroSection/IntroSection.jsx
@@ -1,31 +1,40 @@
-import { NavLink } from 'react-router-dom'
-import './IntroSection.css'
-import { Fade } from 'react-reveal'
+import { NavLink } from "react-router-dom";
+import "./IntroSection.css";
+import { Fade } from "react-reveal";
 
-export const IntroSection =()=>{
-    return(
-
-        <div className="container">
-        
-
-        
-        <div className='IntroSection'>
-            <div className="introSection-left">
-
-               <Fade bottom  delay={50} ><h1>Speak , Write , <span>Succeed</span></h1></Fade> 
-                <Fade bottom delay={100}><p>Lektore provides assimilation pathways for safe migration to English Speaking countries by using language, professional & social tools for migrating professionals, students, Business people and refugees. It also provides elocution & language to anyone wanting to seak better as well as gives actors access to native speakers of languages they need to sound like. Creating a process that converts existing education and professions to transferrable skills that  makes them a credit to their host <br /> countries. </p></Fade>
-               <Fade bottom delay={800}> <div className="introSectionLinks">
-                    <NavLink to='/'>Check Courses</NavLink>
-                    <NavLink to='/'>Learn More</NavLink>
-                </div>
-                </Fade>
-                
+export const IntroSection = () => {
+  return (
+    <div className="container">
+      <div className="IntroSection">
+        <div className="introSection-left">
+          <Fade bottom delay={50}>
+            <h1>
+              Speak , Write , <span>Succeed</span>
+            </h1>
+          </Fade>
+          <Fade bottom delay={100}>
+            <p>
+              Lektore provides assimilation pathways for safe migration to
+              English Speaking countries by using language, professional &
+              social tools for migrating professionals, students, Business
+              people and refugees. It also provides elocution & language to
+              anyone wanting to seek better as well as gives actors access to
+              native speakers of languages they need to sound like. Creating a
+              process that converts existing education and professions to
+              transferrable skills that makes them a credit to their host <br />{" "}
+              countries.{" "}
+            </p>
+          </Fade>
+          <Fade bottom delay={800}>
+            {" "}
+            <div className="introSectionLinks">
+              <NavLink to="/">Check Courses</NavLink>
+              <NavLink to="/">Learn More</NavLink>
             </div>
-            <div className="introSection-right">
-                
-            </div>
+          </Fade>
         </div>
-        
-        </div>
-    )
-}
+        <div className="introSection-right"></div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Testimonial } from "../../../data/Testimonial";
 import "./Testimonials.css";
+import { Fade, Flip } from "react-reveal";
+import AOS from "aos";
+import "aos/dist/aos.css";
 export const Testimonials = () => {
   const { testimonials } = Testimonial();
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
 
   const [first, setFirst] = useState(true);
   const [second, setSecond] = useState(false);
@@ -37,85 +44,134 @@ export const Testimonials = () => {
   };
 
   return (
-    <div className="testimonial">
-      <div className="testi-box-container">
-        {first && (
-          <div className="testi-box">
-            <div className="test-img"></div>
-            <div className="testi-body">
-              <p>
-                I have used quite a number of platforms to acheive my aim but
-                all no avail until a friend introduced me to lektore, with their
-                array of very knowledgeable tutros, i was able to overcome a
-                long term barrier and i made good prgress in English sentences
-                and pronounciation
-              </p>
+    <div className="testimonial-container">
+      <p className="testi-heading">
+        See what some of our customers are saying about <span>LEKTORE</span>
+      </p>
+      <div className="testimonial">
+        <div className="previous">
+          {first && (
+            <div onClick={() => showFourth()}>
+              <img src="./assets/prev.png" alt="" />{" "}
             </div>
-            <div className="testi-name">
-              <h4>Suzie Patricia </h4>
+          )}
+          {second && (
+            <div onClick={() => showFirst()}>
+              <img src="./assets/prev.png" alt="" />{" "}
             </div>
-            <div className="testi-occupation">
-              <h5> Marketer </h5>
+          )}
+          {third && (
+            <div onClick={() => showSecond()}>
+              <img src="./assets/prev.png" alt="" />{" "}
             </div>
-          </div>
-        )}
-        {second && (
-          <div className="testi-box">
-            <div className="test-img"></div>
-            <div className="testi-body">
-              <p>
-                I joined Lektore and my public speaking and overall knowledge in
-                English improved
-              </p>
+          )}
+          {fourth && (
+            <div onClick={() => showThird()}>
+              <img src="./assets/prev.png" alt="" />{" "}
             </div>
-            <div className="testi-name">
-              <h4> Micheal Sandler</h4>
+          )}
+        </div>
+        <div className="testi-box-container">
+          {first && (
+            <div className="testi-box" data-aos="flip-up">
+              <div className="test-img"></div>
+              <div className="testi-body">
+                <p>
+                  I have used quite a number of platforms to acheive my aim but
+                  all no avail until a friend introduced me to lektore, with
+                  their array of very knowledgeable tutros, i was able to
+                  overcome a long term barrier and i made good prgress in
+                  English sentences and pronounciation
+                </p>
+              </div>
+              <div className="testi-name">
+                <h4>Suzie Patricia </h4>
+              </div>
+              <div className="testi-occupation">
+                <h5> Marketer </h5>
+              </div>
             </div>
-            <div className="testi-occupation">
-              <h5>Student</h5>
+          )}
+          {second && (
+            <div className="testi-box" data-aos="flip-up">
+              <div className="test-img"></div>
+              <div className="testi-body">
+                <p>
+                  I joined Lektore and my public speaking and overall knowledge
+                  in English improved
+                </p>
+              </div>
+              <div className="testi-name">
+                <h4> Micheal Sandler</h4>
+              </div>
+              <div className="testi-occupation">
+                <h5>Student</h5>
+              </div>
             </div>
-          </div>
-        )}
-        {third && (
-          <div className="testi-box">
-            <div className="test-img"></div>
-            <div className="testi-body">
-              <p>
-                I have used quite a number of platforms to acheive my aim but
-                all no avail until a friend introduced me to lektore, with their
-                array of very knowledgeable tutros, i was able to overcome a
-                long term barrier and i made good prgress in English sentences
-                and pronounciation
-              </p>
+          )}
+          {third && (
+            <div className="testi-box" data-aos="flip-up">
+              <div className="test-img"></div>
+              <div className="testi-body">
+                <p>
+                  I have used quite a number of platforms to acheive my aim but
+                  all no avail until a friend introduced me to lektore, with
+                  their array of very knowledgeable tutros, i was able to
+                  overcome a long term barrier and i made good prgress in
+                  English sentences and pronounciation
+                </p>
+              </div>
+              <div className="testi-name">
+                <h4> Johnson Adams </h4>
+              </div>
+              <div className="testi-occupation">
+                <h5>Banker</h5>
+              </div>
             </div>
-            <div className="testi-name">
-              <h4> Johnson Adams </h4>
+          )}
+          {fourth && (
+            <div className="testi-box " data-aos="flip-up">
+              <div className="test-img"></div>
+              <div className="testi-body">
+                <p>
+                  I have used quite a number of platforms to acheive my aim but
+                  all no avail until a friend introduced me to lektore, with
+                  their array of very knowledgeable tutros, i was able to
+                  overcome a long term barrier and i made good prgress in
+                  English sentences and pronounciation
+                </p>
+              </div>
+              <div className="testi-name">
+                <h4> Cynthia Bade </h4>
+              </div>
+              <div className="testi-occupation">
+                <h5> Student </h5>
+              </div>
             </div>
-            <div className="testi-occupation">
-              <h5>Banker</h5>
+          )}
+        </div>
+        <div className="next">
+          {first && (
+            <div onClick={() => showSecond()}>
+              <img src="./assets/next.png" alt="" />{" "}
             </div>
-          </div>
-        )}
-        {fourth && (
-          <div className="testi-box">
-            <div className="test-img"></div>
-            <div className="testi-body">
-              <p>
-                I have used quite a number of platforms to acheive my aim but
-                all no avail until a friend introduced me to lektore, with their
-                array of very knowledgeable tutros, i was able to overcome a
-                long term barrier and i made good prgress in English sentences
-                and pronounciation
-              </p>
+          )}
+          {second && (
+            <div onClick={() => showThird()}>
+              <img src="./assets/next.png" alt="" />{" "}
             </div>
-            <div className="testi-name">
-              <h4> Cynthia Bade </h4>
+          )}
+          {third && (
+            <div onClick={() => showFourth()}>
+              <img src="./assets/next.png" alt="" />{" "}
             </div>
-            <div className="testi-occupation">
-              <h5> Student </h5>
+          )}
+          {fourth && (
+            <div onClick={() => showFirst()}>
+              <img src="./assets/next.png" alt="" />{" "}
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
